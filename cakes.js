0599a4724416282f2b89bb12fa119ad700333937
doron <dgescheidt3@gmail.com>
2020-07-29 18:15:12 +0300

    not working

--- cakes.js
+++ cakes.js
@@ -1,18 +1,44 @@
 document.getElementById("cakeMe").addEventListener("click", calcCake);
+//document.getElementsByName("formaType").addEventListener("click", changeForma(this))
+document.getElementsByClassName("input_elem").hidden = true
 
 function calcCake(){
 
-const size = document.getElementById('size').value
+
+
+const diameter = document.getElementById('diameter').value
+const length = document.getElementById('length').value
+const width = document.getElementById('width').value
+const square_edge = document.getElementById('square_edge').value
 const height = document.getElementById('height').value
 const amount = document.getElementById('amount').value
 
-const newSize = document.getElementById('newSize').value
+const newDiameter = document.getElementById('newDiameter').value
+const newlength = document.getElementById('newlength').value
+const newwidth = document.getElementById('newwidth').value
+const newsquare_edge = document.getElementById('newsquare_edge').value
 const newHeight = document.getElementById('newHeight').value
 const newAmount = document.getElementById('newAmount').value
 
-const cakes  = size/2 * size/2 * height * Math.PI * amount
-const newCakes = newSize/2 * newSize/2 * newHeight * Math.PI * newAmount
+
+
+const cakes  = diameter/2 * diameter/2 * height * Math.PI * amount
+const newCakes = newDiameter/2 * newDiameter/2 * newHeight * Math.PI * newAmount
 
 const ratio = newCakes / cakes
  alert (ratio);
 }
+
+function changeForma(elem){
+    document.getElementsByClassName("input_elem").hidden = true
+    if (elem == document.getElementById("round")) {
+         document.getElementsByClassName("round").hidden = false
+    }
+    else if (elem == document.getElementById("square")) {
+        //shows square forma
+    }
+    else  {
+        //shows malben forma
+    }
+}
+
