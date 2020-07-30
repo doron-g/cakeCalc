a4cf2b898cade05f8a8f43b6d5701918e522b1e5
doron <dgescheidt3@gmail.com>
2020-07-30 11:19:28 +0300

    fix the new forma section

--- cakes.js
+++ cakes.js
@@ -12,19 +12,27 @@ const square_edge = document.getElementById('square_edge').value
 const height = document.getElementById('height').value
 const amount = document.getElementById('amount').value
 
-const newDiameter = document.getElementById('newDiameter').value
+const newdiameter = document.getElementById('newdiameter').value
 const newlength = document.getElementById('newlength').value
 const newwidth = document.getElementById('newwidth').value
 const newsquare_edge = document.getElementById('newsquare_edge').value
-const newHeight = document.getElementById('newHeight').value
-const newAmount = document.getElementById('newAmount').value
+const newheight = document.getElementById('newheight').value
+const newamount = document.getElementById('newamount').value
 
 
 
-const cakes  = diameter/2 * diameter/2 * height * Math.PI * amount
-const newCakes = newDiameter/2 * newDiameter/2 * newHeight * Math.PI * newAmount
+const round_cake  = diameter/2 * diameter/2 * height * Math.PI * amount
+const rectangle_cake = length * width * height * amount
+const square_cake = square_edge * square_edge * height * amount
+const new_round_cakes = newdiameter/2 * newdiameter/2 * newheight * Math.PI * newamount
+const new_rectangle_cake = newlength * newwidth * newheight * newamount
+const new_square_cake = newsquare_edge * newsquare_edge * newheight * newamount
 
-const ratio = newCakes / cakes
+//originalcake = get this value by choosing original cake type
+//newcake = get this value by choosing new cake type
+
+
+const ratio = newcake / originalcake
  alert (ratio);
 }
 
@@ -36,6 +44,18 @@ function changeForma(cakeType){
           elem.style.visibility="visible";
         } else {
             elem.style.visibility="hidden";
+        }
+    }
+}
+
+function newchangeForma(cakeType){
+    const newelems = document.getElementsByClassName("new_input_elem");
+    for(i=0; i<newelems.length; i++){
+    const newelem = newelems[i];
+     if(newelem.classList.contains(cakeType)){
+          newelem.style.visibility="visible";
+        } else {
+            newelem.style.visibility="hidden";
         }
     }
 }
