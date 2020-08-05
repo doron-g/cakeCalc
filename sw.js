a1495c33a184f27ae78c662926063034d17f9a81
doron <dgescheidt3@gmail.com>
2020-08-05 18:15:35 +0300

    pwa

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('cakeCalc').then(function(cache) {
+     return cache.addAll([
+       '/cakeCalc/js/cakes.js',
+       '/cakeCalc/img/whisk.png',
+       '/cakeCalc/img/inst16.png',
+       '/cakeCalc/index.html',
+       '/cakeCalc/index.js',
+
+
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
